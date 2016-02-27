e6eb9cbc06c3c0b2698c5c16508ecd3a4c815bf5
zwidny <649038269@qq.com>
2016-02-27 12:06:41 +0800

    angularjs directive: link -> $scope scope = @ &

---
+++ angularjs/angularjs_up_and_running/chap11/directive-with-scope-advanced/scripts/directive.js
@@ -0,0 +1,27 @@
+angular.module('stockMarketApp')
+    .directive('stockWidget', [function () {
+        return {
+            templateUrl: 'views/stock.html',
+            restrict: 'A',
+            scope: {
+                stockData: '=',
+                stockTitle: '@',
+                whenSelect: '&'
+            },
+            link: function ($scope, $element, $attrs) {
+                $scope.getChange = function (stock) {
+                    return Math.ceil(((stock.price - stock.previous) /
+                        stock.previous) * 100);
+                };
+                $scope.onSelect = function () {
+                    $scope.whenSelect({
+                        stockName: $scope.stockData.name,
+                        stockPrice: $scope.stockData.price,
+                        stockPrevious: $scope.stockData.previous
+                    })
+                }
+
+            }
+        }
+
+    }]);
